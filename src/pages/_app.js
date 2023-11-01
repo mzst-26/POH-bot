15c65c73cf76fbfc95d2b1b2f4c66596472a8a68
mobin zaki <1380765@cityplym.ac.uk>
2023-11-01 21:51:23 +0000

    add connect wallet button

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,30 @@
 import '@/styles/globals.css'
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
+
+import { WagmiConfig } from 'wagmi'
+import { arbitrum, mainnet } from 'wagmi/chains'
+
+// 1. Get projectId
+const projectId = '46b15d4ac7df71221bbf8b7299b90b88'
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const chains = [mainnet, arbitrum]
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+     <Component {...pageProps} />
+    </WagmiConfig>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,55 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Link from 'next/link'
+import React, { useEffect, useState } from 'react';
+import { useWeb3Modal } from '@web3modal/wagmi/react'
+
+// import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  // const {} = useAccount()
+  // const {} = useSignMessage()
+
+  // get the open and close hooks 
+  const {open, close} = useWeb3Modal();
+
+
+  //handle button when clicked
+  async function handleConnectWalletButton(){
+    const d =  await open(true)
+    
+  }
+  
   return (
+   
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-hello
+
+    <div className={`flex flex-row items-center justify-between p-24 `}>
+
+      <a 
+          onClick={handleConnectWalletButton}
+          className="border-black group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Connect Wallet{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Connect your wallet and sign a message.
+          </p>
+        </a>
+
+
+      </div>
     </main>
+
+
+    
   )
 }
