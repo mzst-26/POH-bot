d6451c0cf612f3d9c6b048f31caee6a8b3101060
mobin zaki <1380765@cityplym.ac.uk>
2023-11-01 20:29:18 +0000

    edit and clean next app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
+import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -8,7 +8,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-
+hello
     </main>
   )
 }
